b036d171563d7151db41bfb59bd1bb2b16ca7632
ivil2021 <89508514326@mail.ru>
2025-04-29 23:55:37 +0300

    RegistrationForm: rename RegistrationForm.tsx to index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { RegistrationForm } from './components/RegistrationForm/RegistrationForm';
+import { RegistrationForm } from './components/RegistrationForm';
 import { ThemeToggle } from './components/ThemeToggle/ThemeToggle';
 
 export function App() {
